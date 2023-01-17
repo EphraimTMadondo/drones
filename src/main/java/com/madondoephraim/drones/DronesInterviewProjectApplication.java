712eb36db3541f1fdcdc4ddcfc9cafe7f1d4d2a8
Ephraim <madondoephraim@gmail.com>
2023-01-17 15:24:35 +0200

    01 - Initial Commit - Empty Project

---
+++ src/main/java/com/madondoephraim/drones/DronesInterviewProjectApplication.java
@@ -0,0 +1,13 @@
+package com.madondoephraim.drones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronesInterviewProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronesInterviewProjectApplication.class, args);
+	}
+
+}
